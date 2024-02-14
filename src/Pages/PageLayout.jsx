d5a96f05e64340b4557414a44e7ca1e1729947db
Edward Lee <edward@effyjewelry.com>
2024-02-14 11:59:17 -0500

    btn css

--- src/Pages/PageLayout.jsx
+++ src/Pages/PageLayout.jsx
@@ -5,7 +5,7 @@ import { useIsAuthenticated } from "@azure/msal-react";
 import { useNavigate } from "react-router-dom";
 import Carnival from "../Image/Carnival.png";
 import Norwegian from "../Image/NCL.png";
-import DirectionsBoatRoundedIcon from '@mui/icons-material/DirectionsBoatRounded';
+import DirectionsBoatRoundedIcon from "@mui/icons-material/DirectionsBoatRounded";
 //import Particles from '../Particles';
 //import { Button } from '@mui/material';
 
@@ -38,29 +38,35 @@ const BodyPage = () => {
       <div className="btnContainer">
         <div className="glassmorphic">
           <div className="imgBox">
-            <i className="cclPic"><img className="carnivalPic"src={Carnival} alt="Carnival"/></i>
+            <i className="cclPic">
+              <img className="carnivalPic" src={Carnival} alt="Carnival" />
+            </i>
           </div>
           <h3>Carnival Cruise Line Invoice Data</h3>
           <div className="contentBoxCCL">
-            <button onClick={clickCarnival}>Carnival</button>
+            <button className="buttons" onClick={clickCarnival}>Carnival</button>
           </div>
         </div>
         <div className="glassmorphic">
           <div className="imgBox">
-          <i className="nclPic"><img className="norwegianPic"src={Norwegian} alt="Norwegian"/></i>
+            <i className="nclPic">
+              <img className="norwegianPic" src={Norwegian} alt="Norwegian" />
+            </i>
           </div>
           <h3>Norwegian Cruise Line Invoice Data</h3>
           <div className="contentBoxNCL">
-            <button onClick={clickNCL}>Norwegian</button>
+            <button className="buttons" onClick={clickNCL}>Norwegian</button>
           </div>
         </div>
         <div className="glassmorphic">
           <div className="imgBox">
-            <i className="otherPic"><DirectionsBoatRoundedIcon style={{ fontSize: '5.5rem' }}/></i>
+            <i className="otherPic">
+              <DirectionsBoatRoundedIcon style={{ fontSize: "5.5rem" }} />
+            </i>
           </div>
           <div className="contentBoxOther">
             <h3>Other</h3>
-            <button onClick={clickOther}>Other</button>
+            <button className="buttons" onClick={clickOther}>Other</button>
           </div>
         </div>
       </div>
